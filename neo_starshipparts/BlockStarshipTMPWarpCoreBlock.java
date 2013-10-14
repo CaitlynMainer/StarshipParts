f44cbfd3418db9ba37acc3d9b394e19bacbff549
Joshua-Mainer <Joshua.Mainer@gmail.com>
2013-10-13 23:46:03 -0500

    Adding code!

---
+++ neo_starshipparts/BlockStarshipTMPWarpCoreBlock.java
@@ -0,0 +1,21 @@
+package neo_starshipparts;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+
+public class BlockStarshipTMPWarpCoreBlock extends Block {
+
+	public BlockStarshipTMPWarpCoreBlock (int id, Material par2Material) {
+		super(id, par2Material);
+		this.setCreativeTab(Starshipparts.tabCustom);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister par1IconRegister)
+	{
+		this.blockIcon = par1IconRegister.registerIcon(Starshipparts.modid + ":" + (this.getUnlocalizedName().substring(5)));
+	}
+}
